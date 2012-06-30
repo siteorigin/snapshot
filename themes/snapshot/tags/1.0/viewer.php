2b192e32d65a8eace0eae6bbf13d2a5c9263018e
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2012-06-30 19:49:38 +0000

    Deleted Snapshot tag

--- themes/snapshot/tags/1.0/viewer.php
+++
@@ -1,8 +0,0 @@
-<?php if(has_post_thumbnail()) : ?>
-<div id="post-single-viewer">
-	<div class="container">
-		<?php print wp_get_attachment_image(get_post_thumbnail_id(), 'single-large', false, array('class' => 'single-image')); ?>
-	</div>
-</div>
-<?php endif; ?>
-<div id="home-slider-below"></div>
