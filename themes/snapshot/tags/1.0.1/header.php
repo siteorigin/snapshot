694a542720c028ddf89999a1398757c13a867817
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2012-07-27 15:31:52 +0000

    Tagged Snapshot

---
+++ themes/snapshot/tags/1.0.1/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
+	<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale = 1.0, user-scalable=0' />
+
+	<title><?php wp_title('|', true, 'right'); ?></title>
+
+	<link rel="stylesheet" type="text/css" media="screen" href="<?php print get_stylesheet_uri(); ?>" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri() ?>js/html5shiv.js"></script>
+	<![endif]-->
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class() ?>>
+
+<div id="top-area">
+	<div class="container">
+		<div id="logo">
+			<a href="<?php print site_url('/') ?>" title="<?php esc_attr(get_bloginfo('title', 'display') . ' - '.get_bloginfo('description', 'display')) ?>">
+				<?php $header = get_custom_header(); if(!empty($header->url)) : ?>
+					<img src="<?php print esc_attr($header->url) ?>" width="<?php print intval($header->width)?>" height="<?php print intval($header->height)?>" alt="<?php esc_attr(get_bloginfo('title', 'display')) ?>" />
+				<?php else : ?>
+					<h1><em></em><?php bloginfo('title', 'display') ?></h1>
+				<?php endif ?>
+			</a>
+		</div>
+
+		<?php
+		wp_nav_menu(array(
+			'theme_location' => 'main-menu',
+			'fallback_cb' => false,
+			'depth' => 2,
+		));
+		?>
+	</div>
+</div>
+
+<?php if(so_setting('general_search')) get_template_part('premium/searchbar'); ?>
