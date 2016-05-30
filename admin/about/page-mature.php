55f7a711be1c49fb4480ab753831bfdeffc8d3bb
gregpriday <greg@siteorigin.com>
2016-05-30 18:12:49 +0200

    Updates to about page

--- admin/about/page-mature.php
+++
@@ -1,6 +0,0 @@
-<h3><?php _e( 'Stable and Mature', 'snapshot' ) ?></h3>
-<p>
-	<?php _e( "We've been developing Origami since 2012.", 'snapshot' ) ?>
-	<?php _e( "In fact, Origami was our very first WordPress Theme.", 'snapshot' ) ?>
-	<?php _e( "We've continued maintaining and evolving it over all these years - turning it into one of our most stable WordPress themes.", 'snapshot' ) ?>
-</p>
